5c8ecf2d5a0972e6d5f40f8fb1bc07da613f20ce
idbeholdv18 <alimsadullaev18@gmail.com>
2025-03-02 17:24:51 +0300

    initial routing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "@/app/ui/main.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./shared/router/router";
 
 const rootElement = document.getElementById("root");
 
 if (!rootElement) throw Error('cannot find element with id "root"');
 
 const root = createRoot(rootElement);
-root.render(
-  <div>
-    <h1>test</h1>
-  </div>
-);
+root.render(<RouterProvider router={router} />);
