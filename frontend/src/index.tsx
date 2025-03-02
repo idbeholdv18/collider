3a2f7bb3d772d266d7d8f8279e99e91b7a753ab0
idbeholdv18 <alimsadullaev18@gmail.com>
2025-03-02 17:15:49 +0300

    styles setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,13 @@
 import { createRoot } from "react-dom/client";
+import "@/app/ui/main.css";
 
 const rootElement = document.getElementById("root");
 
 if (!rootElement) throw Error('cannot find element with id "root"');
 
 const root = createRoot(rootElement);
-root.render(<div>test</div>);
+root.render(
+  <div>
+    <h1>test</h1>
+  </div>
+);
