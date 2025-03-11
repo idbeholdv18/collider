4404849ccf6cade063358c44789e353fb12a41b3
idbeholdv18 <alimsadullaev18@gmail.com>
2025-03-11 20:50:40 +0300

    feat(frontend): open/close sidebar
    added an open/closed sidebar state with a corresponding ui and switches

--- frontend/src/shared/ui/sidebar/ui/_config.ts
+++ frontend/src/shared/ui/sidebar/ui/_config.ts
@@ -1,3 +1,7 @@
-export const sidebarComponents = {} as const;
+import { SidebarToggle, T_SidebarToggle } from "./Sidebar.Toggle";
+
+export const sidebarComponents = {
+  Toggle: SidebarToggle as T_SidebarToggle,
+} as const;
 
 export type T_SidebarComponents = typeof sidebarComponents;
--- frontend/src/widget/sidebar/ui/Sidebar.tsx
+++ frontend/src/widget/sidebar/ui/Sidebar.tsx
@@ -1,23 +1,35 @@
 import { FilterModule } from "@/app/module/filter/ui/Filter.Module";
 import { ImageModule } from "@/app/module/image/ui/Image.Module";
-import { Sidebar as SharedSidebar } from "@/shared/ui/sidebar";
+import { Sidebar as SharedSidebar, useSidebar } from "@/shared/ui/sidebar";
 import { FC } from "react";
+import { SidebarOpenIcon } from "./Sidebar.Icon.Open";
+import clsx from "clsx";
+import { SidebarCloseIcon } from "./Sidebar.Icon.Close";
 
 export interface SidebarProps {}
 
 const testFilterArray = [1, 2, 3];
 
 export const Sidebar: FC<SidebarProps> = () => {
+  const { isOpen } = useSidebar();
   return (
-    <SharedSidebar>
-      <div className='fixed h-screen w-96 bg-background-contranst border border-e-border-primary pt-14'>
-        <div className='flex flex-col p-3 gap-6'>
-          <ImageModule />
-          {testFilterArray.map((filter, idx) => (
-            <FilterModule key={idx} />
-          ))}
-        </div>
+    <div
+      className={clsx(
+        "fixed h-screen w-80 lg:w-96 bg-background-contranst border border-e-border-primary pt-14 transition-transform",
+        !isOpen && "max-lg:-translate-x-80"
+      )}
+    >
+      <div className='flex flex-col p-3 gap-6'>
+        <SharedSidebar.Toggle
+          className={clsx("lg:hidden absolute top-16 -right-8")}
+          closeIcon={<SidebarCloseIcon />}
+          openIcon={<SidebarOpenIcon />}
+        />
+        <ImageModule />
+        {testFilterArray.map((filter, idx) => (
+          <FilterModule key={idx} />
+        ))}
       </div>
-    </SharedSidebar>
+    </div>
   );
 };
