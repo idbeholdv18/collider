411ba6a9f3d27fc035a899b6e4c50413e1ac3991
idbeholdv18 <alimsadullaev18@gmail.com>
2025-03-15 13:24:20 +0300

    feat(frontend): image scaling
    added scaling pictures feature

--- frontend/src/page/app/page/App.page.tsx
+++ frontend/src/page/app/page/App.page.tsx
@@ -1,7 +1,8 @@
 import { useGetImageByNameQuery } from "@/features/upload_image/api/image.api";
 import { selectImageUrl } from "@/features/upload_image/selectors/imageUrl.selector";
 import { useAppSelector } from "@/shared/store/hooks";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
 export interface AppPageProps {}
 
@@ -10,19 +11,40 @@ export const AppPage: FC<AppPageProps> = () => {
   const { data } = useGetImageByNameQuery(url, {
     skip: !url,
   });
+
+  useEffect(() => {
+    if (data) {
+      const img = new Image(); // Создаем новый объект Image
+      img.src = data; // Устанавливаем источник
+    }
+  }, [data]); // Зависимость от data
+
   return (
-    <div className='min-h-screen lg:ms-96 pt-14'>
-      <div className='flex flex-col justify-between h-full p-3 gap-3'>
-        <div className='rounded-md bg-border-primary p-3 flex justify-center items-center'>
+    <div className='h-screen lg:ms-96 pt-14 flex flex-col'>
+      <TransformWrapper
+        initialScale={1}
+        minScale={0.2}
+        maxScale={6}
+        centerOnInit={true}
+        disablePadding={true}
+      >
+        <TransformComponent
+          wrapperStyle={{ width: "100%", height: "100%", marginBottom: "12px" }}
+        >
           {data ? (
-            <img className='object-scale-down w-full h-full' src={data} h-96 />
+            <img
+              className='object-contain w-full h-full'
+              src={data}
+              alt='Uploaded'
+              style={{ imageRendering: "revert-layer" }}
+            />
           ) : (
             "upload"
           )}
-        </div>
-        <div className='p-3 rounded-md outline outline-border-primary bg-background-contranst'>
-          <h1>Toolbar</h1>
-        </div>
+        </TransformComponent>
+      </TransformWrapper>
+      <div className='p-3 mb-3 mx-3 rounded-md outline outline-border-primary bg-background-contranst'>
+        <h1>Toolbar</h1>
       </div>
     </div>
   );
